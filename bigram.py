edad40df95d192f01b716fb4df584e22db00d809
Coderrexe <shizimo@gmail.com>
2025-07-25 15:48:58 +0800

    change number of steps

--- bigram.py
+++ bigram.py
@@ -5,7 +5,7 @@ from torch.nn import functional as F
 # Hyperparameters
 batch_size = 32
 block_size = 8
-max_iters = 3000
+max_iters = 20000
 eval_interval = 300
 # A typical good learning rate is 3e-4, but for small networks like this higher LR is fine.
 learning_rate = 1e-2
