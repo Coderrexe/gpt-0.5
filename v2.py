77f34024d4a131ce0ddc5e02d21acb6edc240480
Coderrexe <shizimo@gmail.com>
2025-07-25 23:45:01 +0800

    multi head self attention

--- v2.py
+++ v2.py
@@ -98,6 +98,17 @@ class Head(nn.Module):
         v = self.value(x) # (B, T, C)
         out = wei @ v # (B, T, T) @ (B, T, C) -> (B, T, C)
         return out
+    
+
+class MultiHeadAttention(nn.Module):
+    """ multiple heads of self-attention in parallel """
+    
+    def __init__(self, num_heads, head_size):
+        super().__init__()
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+
+    def forward(self, x):
+        return torch.cat([h(x) for h in self.heads], dim=-1)
 
 
 class BigramLanguageModel(nn.Module):
@@ -109,7 +120,7 @@ class BigramLanguageModel(nn.Module):
         # they are in the sequence, since self-attention and aggregation can be thought of as
         # a directed graph where all previous nodes point to the next token node.
         self.position_embedding_table = nn.Embedding(block_size, n_embd)
-        self.sa_head = Head(n_embd)
+        self.sa_heads = MultiHeadAttention(4, n_embd//4) # 4 heads of 8-dimentional self-attention
         self.lm_head = nn.Linear(n_embd, vocab_size)
     
     def forward(self, idx, targets=None):
@@ -119,7 +130,7 @@ class BigramLanguageModel(nn.Module):
         tok_emb = self.token_embedding_table(idx)  # (batch_size, block_size, vocab_size)
         pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T, C)
         x = tok_emb + pos_emb # (B, T, C)
-        x = self.sa_head(x) # apply 1 head of self-attention
+        x = self.sa_heads(x) # apply 1 head of self-attention
         logits = self.lm_head(x) # (B, T, vocab_size)
         
         if targets == None:
