b67682b7deb3a5dd7dbee033a8aaf4a54b19d2af
Coderrexe <shizimo@gmail.com>
2025-07-26 00:37:23 +0800

    finish gpt implementation

--- gpt.py
+++ gpt.py
@@ -94,6 +94,7 @@ class Head(nn.Module):
         q = self.query(x) # (B, T, C)
 
         # compute attention scores (affinities)
+        # algorithm given in the original paper
         wei = q @ k.transpose(-2, -1) * C**-0.5 # (B, T, C) @ (B, C, T) -> (B, T, T)
         wei = wei.masked_fill(self.tril[:T, :T] == 0, float("-inf")) # (B, T, T)
         wei = F.softmax(wei, dim=-1) # (B, T, T)
@@ -156,7 +157,7 @@ class Block(nn.Module):
         return x
 
 
-class BigramLanguageModel(nn.Module):
+class GPT(nn.Module):
     def __init__(self):
         super().__init__()
         # Each token directly reads off the logits for the next token from a lookup table.
@@ -207,7 +208,7 @@ class BigramLanguageModel(nn.Module):
         return idx
 
 
-model = BigramLanguageModel()
+model = GPT()
 model.to(device)
 
 optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
@@ -232,3 +233,129 @@ print(loss.item())
 context = torch.zeros((1, 1), dtype=torch.long, device=device)
 out = decode(model.generate(context, max_new_tokens=500)[0].tolist())
 print(out)
+
+"""
+loss: ~1.46
+trained for 15 minutes on A100
+
+The top in a world by susphoring grace.
+
+LUCIO:
+We muse hath resistes him so sovere: son't his other wrough
+stands of coverent sh'd: he has here, and stand it
+and poor exceeder or a Henry's last, stay
+not in faith, forewell's base of graves, thanks, happy comparel,
+warmentfully: may as face by the courst, that strangth
+errise hath breathed. Hastings come to Valenting.
+
+HERMIONE:
+Well have been bolly poor late
+Is the lords.
+
+ABELLA:
+Let's found: I will kind him;
+I do braw'sy him business wherein far his face.
+
+LUCENTIO:
+He is last afford: make him diseably to London,
+Take him great Hastings, boldness in his natic keeps,
+To oftragn lost me ready glust through the house.
+Why chose that I dares it be a Montague.
+
+MONTAGUE:
+Woe's Claudly Haste of his own at last the Volscient,
+And seen'd helpit: bearn to do it be, and most hop,
+Miscause's more conterar than without this lambs
+Shall down appla fortune flight flowers.
+
+FRIAR LAUAURENCE:
+His son, do your morself, that leaven your honours
+Sufferable in more and suffer five.
+A horse! High-graced York rights. And bother Montague
+That the caapter, that I soughd him; such a chooson
+Woes, that they have splight that care
+Fades the respect to her spult: betfore him,
+Un tell him up hine, or hope, that throw'st thou carry
+apied sing with wear over the plenting long stamper
+That doth butcherity. For love, what arful was an soldier
+That last twain of all and Romeo runly Froth.
+
+VALHASINA:
+Nobleman; go, then both groans to us.
+
+AUFIDIUS:
+O those prepation!
+
+AUFIDIUS:
+It is: ever crimty be a house.
+
+Second Citizen:
+We give heed.
+
+All Clarence, that makes not know work. The may say speak way.
+How is my sorrow to strange on the fares
+That which to play some called Margaret
+The state town outward's wife, as the foul sleep;
+Trickly of from thy blod'sty day blows here,
+And pratess that chrospiles stalk falls up,
+The world's hollow princhment, which should a bankind,
+At till naKaina-daughter tae truth,
+Craged from lares an oar that rems' stol-eat with blass.
+Those is sometimes well call the Tale, the rod,
+Submished his truth; Right states; but for ourselves,
+Claud not thy hand, addingness.
+No, there all conslent here pue the fault that yokUCHastisful
+From servant and folling 'em how that: be drunk,
+Set was halt be else, I will betwixt thee three with Tewar:
+I am their man before a vile bad amiss'd
+And thought have shorn'd the back-flowed of mine,
+And ne'er than this, they leave spectiff.
+I am to sure,
+To maintain on what rash thy dam of suddise!
+Thyself thee pays wither edge.
+God I am speak to-morrow's like, to me speak,
+Am
+Dash your deliverance, nitted tongue to study.
+But if you were could not love, if you such commands,
+Your ignoration lightnifies
+Sufficed hath granted a sacret
+Divine: minute hath too should be assured,
+Unless, heaven to themselvish, as I am,
+Hance my father bend to them speak;
+His the business' hath themselves;
+For his ordance: bow his hand, hell pluck my pet!
+What it brace there of his oath?
+Rather? Where, whilst thou garling feet Bark? aim? stay;
+So if He and him come, and make his mostake
+You forbid you had stoopp'd your grace.
+
+Servant:
+He may once it indeces:
+See do it between.
+
+Provost:
+Ah, sir! shall it stay the heavy nights.
+
+PeRDINA:
+Behind-foot, sir; three manner did he remiss
+no slain up is disconful: slave you breast-wish.
+
+HUMIO:
+Why more lose on than ime well so fofter townd
+you.
+
+LUCIO:
+How find, I must by our son?
+
+PRINCE EDWARD:
+Not I, sir?
+
+PETHUMIO:
+Base my fa-lor; I have ports to guilty:
+It string is remorse: seldiers, thou retirest that Titus;
+And I will have my close father- place:
+I have well kings your husband; he will flow.
+
+FRIAR:
+I am nother for your highness' remain.
+"""
